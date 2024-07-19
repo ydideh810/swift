188390b06c213844ef2d87bd76232637e81020f3
ydideh810 <yd.baller810@gmail.com>
2024-07-19 12:24:42 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ import { Toaster } from "sonner";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
-	title: "Swift",
+	title: "Haddal",
 	description:
-		"A fast, open-source voice assistant powered by Groq, Cartesia, and Vercel.",
+		"Your private voice assistant. Powered by Groq & Cartesia.",
 };
 
 export default function RootLayout({
