66d79cfa19f06115decddfdc83587264e93f564a
ydideh810 <yd.baller810@gmail.com>
2024-07-20 00:19:10 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -143,7 +143,7 @@ export default function Home() {
 				    height: '218px',
 				    position: 'absolute',
 				    left: '50%',
-				    top: '0',
+				    top: '10',
 			            
 				    transform: 'translate(-50%, -50%)'
  				 }} />
